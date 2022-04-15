027b6dc79b138b86b3dfc08e271dbe4cac932999
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-04-15 17:56:33 +0100

    Added initial styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
+import './styles.css';
+
 function App() {
   return (
     <div className='calculator-grid'>
-      <div className='output'>
+      <div className='output' id='display'>
         <div className='previous-operand'></div>
         <div className='current-operand'></div>
       </div>
-      <button className='span-two'>AC</button>
+      <button className='span-two' id='clear'>
+        AC
+      </button>
       <button>DEL</button>
-      <button>/</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
-      <button className='span-two'>=</button>
+      <button id='divide'>/</button>
+      <button id='one'>1</button>
+      <button id='two'>2</button>
+      <button id='three'>3</button>
+      <button id='multiply'>*</button>
+      <button id='four'>4</button>
+      <button id='five'>5</button>
+      <button id='six'>6</button>
+      <button id='add'>+</button>
+      <button id='seven'>7</button>
+      <button id='eight'>8</button>
+      <button id='nine'>9</button>
+      <button id='subtract'>-</button>
+      <button id='decimal'>.</button>
+      <button id='zero'>0</button>
+      <button className='span-two' id='equals'>
+        =
+      </button>
     </div>
   );
 }
