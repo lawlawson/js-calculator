94fdf0fc8aac9f4c28f91ae9dd271068bf4bea48
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-04-20 00:03:58 +0100

    Added actions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import { useReducer } from 'react';
 import './styles.css';
 
-function reducer(state, action) {}
+const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate',
+};
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${currentOperand || ''}${payload.digit}`,
+      };
+  }
+}
 
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
@@ -9,10 +25,13 @@ function App() {
     {}
   );
 
+  dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 } });
   return (
     <div className='calculator-grid'>
       <div className='output' id='display'>
-        <div className='previous-operand'>{previousOperand}</div>
+        <div className='previous-operand'>
+          {previousOperand} {operation}
+        </div>
         <div className='current-operand'>{currentOperand}</div>
       </div>
       <button className='span-two' id='clear'>
