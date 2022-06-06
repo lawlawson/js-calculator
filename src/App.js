e8d0ea2efad54d2e09240a253f9aa81d64c28898
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-06-06 22:45:23 +0100

    Removed code for refactor

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
-import { useReducer } from 'react';
-// import { DigitButton } from './DigitButton';
 import './styles.css';
 
-export const ACTIONS = {
-  ADD_DIGIT: 'add-digit',
-  CHOOSE_OPERATION: 'choose-operation',
-  CLEAR: 'clear',
-  DELETE_DIGIT: 'delete-digit',
-  EVALUATE: 'evaluate',
-};
-
-function reducer(state, { type, payload }) {
-  switch (type) {
-    case ACTIONS.ADD_DIGIT:
-      return {
-        ...state,
-        currentOperand: `${currentOperand || ''}${payload.digit}`,
-      };
-  }
-}
-
 function App() {
-  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
-    reducer,
-    {}
-  );
-
   return (
     <div className='calculator-grid'>
       <div className='output' id='display'>
-        <div className='previous-operand'>
-          {previousOperand} {operation}
-        </div>
-        <div className='current-operand'>{currentOperand}</div>
+        <div className='previous-operand'></div>
+        <div className='current-operand'></div>
       </div>
       <button className='span-two' id='clear'>
         AC
